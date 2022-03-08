5fc2d3871aff025cb86b4ddeccd71c27b15f5483
nischal6878 <chauhannischal4@gmail.com>
2022-03-08 20:07:05 +0530

    file added

---
+++ String.py
@@ -0,0 +1,39 @@
+str_ex = 'Softvan_innoventa'
+
+print(str_ex)
+# Softvan_innoventa
+
+'''
+comment section
+comment1
+comment2
+'''
+
+print(str_ex[0])
+# S
+
+# Slicing Operation
+print(str_ex[2:7])
+# ftvan
+
+
+print(str_ex[2:])
+#ftvan_innoventa
+
+print(str_ex * 2)
+# Softvan_innoventaSoftvan_innoventa
+
+print(str_ex + "Hello")
+# Softvan_innoventaHello
+
+print(str_ex, 'hello')
+# Softvan_innoventa hello
+
+print(str_ex + '1')
+# Softvan_innoventa1
+
+print(str_ex, 1)
+# Softvan_innoventa 1
+
+# Format method
+print("Hey My name is:")
